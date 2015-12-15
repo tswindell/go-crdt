be6e0fb1dc9056ee2754395a9eefccc93df47e57
Tom Swindell <t.swindell@rubyx.co.uk>
2015-12-15 10:16:57 +0000

    Updated AES crypto tests.
    Signed-off-by: Tom Swindell <t.swindell@rubyx.co.uk>

--- db/aes_crypto_test.go
+++ db/aes_crypto_test.go
@@ -27,27 +27,30 @@ import (
     "testing"
 
     "bytes"
-    "fmt"
+    "strings"
 )
+
+func TestAESGenerateKey(t *testing.T) {
+    method, _ := NewAESCryptoMethod(AES_256_KEY_SIZE)
+    key := method.GenerateKey()
+
+    parts := strings.SplitN(string(key), ":", 2)
+
+    if parts[0] != key.TypeId() {
+        t.Errorf("Key format error, missing type prefix. %s != %s", parts[0], key.TypeId())
+    }
+}
 
 func TestAESEncryptionMethod(t *testing.T) {
     method, _ := NewAESCryptoMethod(32)
     orig := []byte("Hello, world!")
     key := method.GenerateKey()
 
-    fmt.Printf("KEY LENGTH: %d\n", len(key))
-
-    fmt.Printf("ORIG: %x\n", orig)
-
     text, e := method.Encrypt(key, orig)
     if e != nil { t.Error(e) }
 
-    fmt.Printf("TEXT: %x\n", text)
-
     result, e := method.Decrypt(key, text)
     if e != nil { t.Error(e) }
-
-    fmt.Printf("DEC: %x\n", result)
 
     if !bytes.Equal(result, orig) {
         t.Error("Encryption failed!")
